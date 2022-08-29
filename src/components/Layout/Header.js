e2463361211048abe31df1dbc3c0312fe25e0759
Helga-sov <sandu.helga@gmail.com>
2022-08-29 12:03:06 +0200

    created a Cart reducer and used Refs

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
 import styles from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
 	return (
 		<React.Fragment>
 			<header className={styles.header}>
 				<h1>ReactMeals</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 			<div className={styles["main-image"]}>
 				<img src={mealsImage} alt="A table full of delicious food" />
